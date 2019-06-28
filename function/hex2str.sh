e207103f84d64cea654948c982db6c697a3e9dbd
Cedric Sun <sunce@ufl.edu>
2019-06-28 21:30:51 +0800

    add reference

--- function/hex2str.sh
+++ function/hex2str.sh
@@ -1,3 +1,5 @@
+# see
+# https://stackoverflow.com/questions/13160309/conversion-hex-string-into-ascii-in-bash-command-line
 hex2str() {
 	if [[ $# -ne 1 ]]; then
 		>&2 echo "Usage: hex2str HEX_STRING"
